47c38b102d94d593ce509741d29003b8ee059fd5
Mr-Bojangles <20761167+Mr-Bojangles@users.noreply.github.com>
2026-01-02 17:22:57 -0500

    Add game constants and logging logic

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
 
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
 VERSION = pygame.version.ver
 
 
 def main():
     print(f"Starting Asteroids with pygame version: {VERSION}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
